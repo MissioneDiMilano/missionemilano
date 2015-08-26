ef2335e9a741085b6aa9879313766c610b33b889
Missione di Milano <missionemilano.officeelders@gmail.com>
2015-08-26 17:53:02 +0200

    trying to gitignore zedstate

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -2,3 +2,12 @@
 //# All this logic will automatically be available in application.js.
 //# You can use CoffeeScript in this file: http://coffeescript.org/
 
+
+// Check we are on the right page and go!
+if (window.location.pathname == "/admin/users"){
+    
+    
+    
+    
+    
+}
